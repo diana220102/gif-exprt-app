1b31da2038b6a4c42f09b7719a6ee4accfcd5d9c
Diana Ramirez <dianitrix251017@gmail.com>
2022-03-03 20:46:38 -0600

    Componente AddCategory

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,23 +1,21 @@
 import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
 
-    //const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
     const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
 
-    const handleAdd = () => {
-        // setCategories( [ 'HunterXHunter', ...categories] );
-        setCategories( cats => [ ...cats, 'HunterXHunter' ] );
-    }
-
+    // const handleAdd = () => {
+    //    // setCategories( [ 'HunterXHunter', ...categories] );
+    //    setCategories( cats => [ ...cats, 'HunterXHunter' ] );
+    // }
 
 
     return (
         <>
             <h2>GifExpertApp</h2>
+            <AddCategory />
             <hr />
-
-            <button onClick={ handleAdd }>Agregar</button>
 
             <ol>
                 {
