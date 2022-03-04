6c6d69fdf548b43d99a3d690fe72e31d94263a6d
Diana Ramirez <dianitrix251017@gmail.com>
2022-03-03 21:20:41 -0600

    useEffect

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 export const GifGrid = ({ category }) => {
+
+    const [count, setCount] = useState(0);
+
+    useEffect( () => {
+        getGifs();
+    }, [])
+
 
     const getGifs = async() => {
 
@@ -17,14 +24,15 @@ export const GifGrid = ({ category }) => {
         })
 
         console.log(gifs);
+        // setImgs
 
     }
-
-    getGifs();
     
     return (
         <div>
             <h3> { category } </h3>
+            <h3> { count } </h3>
+            <button onClick={ ()=> setCount( count + 1 )}></button>
         </div>
     )
 }
