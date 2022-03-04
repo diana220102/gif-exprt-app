18670f3eadaebdd5f05212479af33df9ca1eef29
Diana Ramirez <dianitrix251017@gmail.com>
2022-03-03 21:17:00 -0600

    Fetch API - Obtener las imágenes

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,15 +1,10 @@
 import React, { useState } from 'react'
 import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
 
-    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
-
-    // const handleAdd = () => {
-    //    // setCategories( [ 'HunterXHunter', ...categories] );
-    //    setCategories( cats => [ ...cats, 'HunterXHunter' ] );
-    // }
-
+    const [categories, setCategories] = useState(['One Punch']);
 
     return (
         <>
@@ -20,7 +15,10 @@ export const GifExpertApp = () => {
             <ol>
                 {
                     categories.map( category  => {
-                        return <li key={ category }> { category } </li>
+                        <GifGrid
+                            key={ category }
+                            category={ category }
+                        />
                     })
                 }
             </ol>
