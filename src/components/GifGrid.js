4d2d1c5de7b9cdf4159317b02d39cc6548d74954
Diana Ramirez <dianitrix251017@gmail.com>
2022-03-03 21:32:21 -0600

    Fetch API - Obtener las imágenes deseadas

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
-    const [count, setCount] = useState(0);
+    const [images, setImages] = useState([]);
 
     useEffect( () => {
         getGifs();
@@ -11,7 +12,7 @@ export const GifGrid = ({ category }) => {
 
     const getGifs = async() => {
 
-        const url = 'https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=4uETLMDo41Ndasps3vwvN9cu1xWlID2J';
+        const url = 'https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=ozGInY0EtPoDdBKeTjb16BgWdvRo5KI7';
         const resp = await fetch( url );
         const { data } = await resp.json();
 
@@ -24,15 +25,23 @@ export const GifGrid = ({ category }) => {
         })
 
         console.log(gifs);
-        // setImgs
+        setImages( gifs );
 
     }
     
     return (
         <div>
             <h3> { category } </h3>
-            <h3> { count } </h3>
-            <button onClick={ ()=> setCount( count + 1 )}></button>
+    
+                {
+                    images.map( img  => (
+                        <GifGridItem 
+                            key={ img.id }
+                            { ...img } 
+                        />
+                    ))
+                }
+                
         </div>
     )
 }
